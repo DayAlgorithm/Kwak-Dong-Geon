416700839d22c513e5b1b433f80209c7568df600
LloydKwak <kwaklloyd@gmail.com>
2025-05-08 11:39:00 +0900

    목 2179(baekjoon) C++ 풀이 update

--- 4. 목/2179(baekjoon).cpp
+++ 4. 목/2179(baekjoon).cpp
@@ -3,18 +3,22 @@
 
 using namespace std;
 
+const int INF = 987654321;
+
 int n, cnt;
-pair<string, string> ans;
+pair<int, int> ans = {INF, INF};
 vector<string> words;
 
-void findAns(int start){
-    for(int i = start + 1; i < n; ++i){
-        int idx = 0;
-        while(words[start][idx] == words[i][idx]) idx++;
-        if(cnt < idx){
-            cnt = idx;
-            ans = {words[start], words[i]};
+void findAns(){
+    for(int i = 100; i >= 0; --i){
+        map<string, vector<int>> m;
+        for(int j = 0; j < n; ++j){
+            if(words[j].size() < i) continue;
+            string sub = words[j].substr(0, i);
+            if(m.count(sub)) ans = min(ans, {m[sub][0], j});
+            m[sub].push_back(j);
         }
+        if(ans.first != INF) return;
     }
 }
 
@@ -25,9 +29,7 @@ int main(){
     for(int i = 0; i < n; ++i){
         cin >> words[i];
     }
-    for(int i = 0; i < n; ++i){
-        findAns(i);
-    }
-    cout << ans.first << "\n" << ans.second;
+    findAns();
+    cout << words[ans.first] << "\n" << words[ans.second];
     return 0;
 }
