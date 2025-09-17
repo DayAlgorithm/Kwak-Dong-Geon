57f2f9154ddf8e8df5643db4b87db390584acee5
LloydKwak <kwaklloyd@gmail.com>
2025-09-17 11:31:08 +0900

    수 1092(baekjoon) C++ 풀이

---
+++ 3. 수/1092(baekjoon).cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+using namespace std;
+
+int main() {
+    FastIO;
+    int n, m;
+    cin >> n;
+    vector<int> cranes(n);
+    for (int i = 0; i < n; ++i) cin >> cranes[i];
+    cin >> m;
+    vector<int> boxes(m);
+    for (int i = 0; i < m; ++i) cin >> boxes[i];
+
+    sort(cranes.rbegin(), cranes.rend());
+    sort(boxes.rbegin(), boxes.rend());
+
+    if (boxes[0] > cranes[0]) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    vector<int> pos(n, 0);
+    vector<bool> checked(m, false);
+    int moved = 0, ans = 0;
+    while(moved < m){
+        int curMove = 0;
+        for(int i = 0; i < n; ++i){
+            while(pos[i] < m){
+                if(!checked[pos[i]] && cranes[i] >= boxes[pos[i]]){
+                    checked[pos[i]] = true;
+                    ++pos[i]; ++curMove; ++moved;
+                    break;
+                }
+                ++pos[i];
+            }
+        }
+        ++ans;
+    }
+    cout << ans << endl;
+    return 0;
+}
