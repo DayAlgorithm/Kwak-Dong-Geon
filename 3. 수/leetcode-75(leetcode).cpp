273d408b5026be585a79ad11c75e6ca590f8d5ef
LloydKwak <kwaklloyd@gmail.com>
2026-01-14 16:07:24 +0900

    수 leetcode-75(leetcode) C++ 풀이

---
+++ 3. 수/leetcode-75(leetcode).cpp
@@ -0,0 +1,18 @@
+class StockSpanner {
+private:
+    stack<pair<int, int>> s;
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int day = 1;
+        while(!s.empty() && s.top().first <= price) {
+            day += s.top().second;
+            s.pop();
+        }
+        s.push({price, day});
+        return day;
+    }
+};
